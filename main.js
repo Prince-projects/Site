2695136c18196014b709342395485e7693131582
Karl Ziviani <73142421+Prince-projects@users.noreply.github.com>
2021-08-05 11:47:12 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 
+function changeText() {
+  document.getElementById("test").innerHTML = "test2";
+}
+var countVar = 0;
+function timeOnPage() {
+ document.getElementById("counter").innerHTML = countVar + "seconds";
+ countVar++;
+}
+
+setInterval(timeOnPage, 1000)
