812cb0eb3bab0a083f58c6ed7c24b21bb7ed6154
Karl Ziviani <73142421+Prince-projects@users.noreply.github.com>
2021-08-05 11:52:09 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-
 function changeText() {
   document.getElementById("test").innerHTML = "test2";
 }
 var countVar = 0;
+var focused = document.hasFocus()
 function timeOnPage() {
  document.getElementById("counter").innerHTML = countVar + "seconds";
  countVar++;
 }
 
+function checkFocus() {
+if (focused) {
+ document.getElementById("html").blur()
+}
+ 
+}
+
 setInterval(timeOnPage, 1000)
+setInterval(checkFocus, 1000)
